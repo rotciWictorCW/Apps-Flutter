2184d2cd05ac53f7d27bb7b21d7a09627adfbe90
Victor <victorhscampos@gmail.com>
2022-05-19 17:38:27 -0300

    deletando repositorios iniciais

--- meu_primeiro_projeto/android/app/src/main/kotlin/com/example/meu_primeiro_projeto/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.meu_primeiro_projeto
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- primeiro_projeto/android/app/src/main/kotlin/com/rotciwictor/firstproject/primeiro_projeto/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.rotciwictor.firstproject.primeiro_projeto
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
