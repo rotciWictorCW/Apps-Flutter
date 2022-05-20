0dbd4d3f0a4636eabdc9527a084fa552c287bbcf
Victor <victorhscampos@gmail.com>
2022-05-20 11:28:36 -0300

    aula Stateless & Stateful

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -6,11 +6,68 @@ void onPressed(){
 
 void main (){
 
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Scaffold(
+    home: HomeStateful(),
+  ));
+
+}
+
+class HomeStateful extends StatefulWidget {
+  const HomeStateful({Key? key}) : super(key: key);
+
+  @override
+  State<HomeStateful> createState() => _HomeStatefulState();
+}
+
+class _HomeStatefulState extends State<HomeStateful> {
+
+  var _titulo = "Frases do dia";
+  var _texto = "Victor Campos";
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
       appBar: AppBar(
-        title: const Text("Frases do dia"),
+        title: Text( _titulo ),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+                onPressed: (){
+                  setState(() {
+                    _texto = "Curso Flutter";
+                  });
+
+
+                },
+                color: Colors.blueAccent,
+                child: Text("Clique aqui"),
+
+            ),
+            Text("Nome: $_texto")
+          ],
+      ),
+      ),
+    );
+  }
+}
+
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    var _titulo = "Frases do dia";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text( _titulo ),
         backgroundColor: Colors.blueAccent,
       ),
       body: const Padding(
@@ -18,20 +75,18 @@ void main (){
         child: Text("conteúdo princioal"),
       ),
       bottomNavigationBar: BottomAppBar(
-        color: Colors.lightBlue,
-        child: Padding(
+          color: Colors.lightBlue,
+          child: Padding(
             padding: const EdgeInsets.all(16),
             child: Row(
-              children: <Widget>[
-                const Text('Conteúdo princioal'),
-                const Text('Outro Conteúdo'),
+              children: const <Widget>[
+                Text('Conteúdo princioal'),
+                Text('Outro Conteúdo'),
 
               ],
             ),
-        )
-
+          )
       ),
-    ),
-  ));
-
+    );
+  }
 }
