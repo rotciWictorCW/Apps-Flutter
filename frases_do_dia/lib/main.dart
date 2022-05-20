0ec69bb38322be221155b329ce3d0a2084705e1f
Victor <victorhscampos@gmail.com>
2022-05-20 14:08:51 -0300

    app 'finalizado'

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main (){
@@ -17,20 +18,39 @@ class HomeStateful extends StatefulWidget {
 
 class _HomeStatefulState extends State<HomeStateful> {
 
-  var _titulo = "Frases do dia";
-  var _texto = "iuGAUGSDIUGDIGDAUIGAUGDIAdUSihsidhodhwiohdsohdohdoshhdohsdhoidhsodygdifugudgifgiufgigfidsgfsigifg";
+  String _title = "Frases do dia";
+  String _selectedPhrase = "Clique para gerar uma frase";
+  var _phrases = [
+    'Acredite em si próprio e chegará um dia em que os outros não terão outra escolha senão acreditar com você.',
+    "Mudar pode dar medo, mas é uma aventura que pode te levar muito longe",
+    "Tenha coragem para se tornar aquilo que sonha.",
+    "Há tanta coisa deliciosa para descobrir sobre você. Desafie-se, aventure-se e coloque-se em situações novas.",
+    "Minha missão na vida não é apenas sobreviver, mas prosperar.",
+    "Jamais se esqueça: você é o motivo do sorriso de muitas pessoas.",
+    "Não tenho medo de aceitar que você não é mais o mesmo.",
+    "Os sonhos servem para abrir o caminho e mostrar a direção.",
+    "Um dia tudo isto ainda vai parecer pequeno, porque tuas conquistas te farão enorme.",
+    "Quando estiver em dúvida sobre o caminho, faça um pequeno primeiro movimento. Aos poucos, os próximos passos começarão a ficar mais visíveis."
+  ];
+  var _random;
+
+  void randomize(){
+
+  }
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text( _titulo ),
+        title: Text( _title ),
         backgroundColor: Colors.green,
       ),
       body: Container(
+            width: double.infinity,
             margin: EdgeInsets.all(12),
         child: Column(
+
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
@@ -38,23 +58,23 @@ class _HomeStatefulState extends State<HomeStateful> {
                 "images/logo.png",
             ),
             Text(
-              "Nome: $_texto",
-              textAlign: TextAlign.justify,
+              _selectedPhrase,
+              textAlign: TextAlign.center,
               style:TextStyle(
                 fontSize: 20,
                 color: Colors.grey,
                 wordSpacing: 2,
-
               ) ,
             ),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
-                primary: Colors.green, // background
-                onPrimary: Colors.white, // foreground
+                primary: Colors.green,
+                onPrimary: Colors.white,
               ),
               onPressed: (){
                 setState(() {
-
+                  _random = Random().nextInt(10);
+                  _selectedPhrase = _phrases[_random];
                 });
               },
               child: Text(
