a081d3625cfb86f2397e503a55dc51c1d76c8fe8
Victor <victorhscampos@gmail.com>
2022-05-19 21:22:39 -0300

    aula espaçamentos

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -7,22 +7,30 @@ void onPressed(){
 void main (){
 
   runApp(MaterialApp(
+    //debugShowCheckedModeBanner: false,
     title: 'Frase do dia',
     home: Container(
-      color: Colors.white,
-      child: Column(
+      //color: Colors.white,
+      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+      margin: EdgeInsets.only(top:50),
+      decoration: BoxDecoration(
+        border: Border.all(width: 5,color: Colors.deepPurple)
+      ),
+      child: Row(
         children: const [
-          FlatButton(
-            onPressed: onPressed,
-            child: Text(
-                "Clique aqui!",
-                 style: TextStyle(
-                   fontSize: 20,
-                   color: Colors.black,
-                   decoration: TextDecoration.none
-                 ),
-            )
-          )
+          Text("T1"),
+
+          Padding(
+              padding: EdgeInsets.all(50),
+              child:Text("T2"),
+          ),
+
+
+          Text("T3")
+          /*Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed elit nec quam laoreet efficitur.",
+              textAlign: TextAlign.justify,
+          )*/
         ],
       ),
     )
